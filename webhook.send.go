7095a04cc7200837ac91db1d057b2729328508fc
李光春 <gc@dtapp.net>
2022-06-05 11:59:12 +0800

    - 优化入参

--- webhook.send.go
+++ webhook.send.go
@@ -27,9 +27,9 @@ func NewWebhookSendResult(result WebhookSendResponse, body []byte, http goreques
 
 // WebhookSend 发送应用消息
 // https://developer.work.weixin.qq.com/document/path/90372
-func (app *App) WebhookSend(notMustParams ...Params) *WebhookSendResult {
+func (app *App) WebhookSend(notMustParams ...gorequest.Params) *WebhookSendResult {
 	// 参数
-	params := app.NewParamsWith(notMustParams...)
+	params := gorequest.NewParamsWith(notMustParams...)
 	// 请求
 	request, err := app.request(fmt.Sprintf("https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=%s&type=%s", app.key, "text"), params)
 	// 定义
