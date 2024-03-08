e5f8fe97e8dbe99605bc49c82b41dbee3f8af580
dtapps <li.gc@icloud.com>
2024-03-08 10:16:54 +0800

    - update

--- version.go
+++ version.go
@@ -1,5 +1,5 @@
 package wechatqy
 
 const (
-	Version = "1.0.18"
+	Version = "1.0.19"
 )
--- webhook.send.go
+++ webhook.send.go
@@ -41,19 +41,3 @@ func (c *Client) WebhookSend(ctx context.Context, key string, Type string, notMu
 	err = gojson.Unmarshal(request.ResponseBody, &response)
 	return newWebhookSendResult(response, request.ResponseBody, request), err
 }
-
-// WebhookSendText 发送消息文本版
-// https://developer.work.weixin.qq.com/document/path/90372
-func (c *Client) WebhookSendText(ctx context.Context, key string, notMustParams ...gorequest.Params) (*WebhookSendResult, error) {
-	// 参数
-	params := gorequest.NewParamsWith(notMustParams...)
-	// 请求
-	request, err := c.request(ctx, apiUrl+fmt.Sprintf("/cgi-bin/webhook/send?key=%s&type=%s", key, "text"), params, http.MethodPost)
-	if err != nil {
-		return newWebhookSendResult(WebhookSendResponse{}, request.ResponseBody, request), err
-	}
-	// 定义
-	var response WebhookSendResponse
-	err = gojson.Unmarshal(request.ResponseBody, &response)
-	return newWebhookSendResult(response, request.ResponseBody, request), err
-}
